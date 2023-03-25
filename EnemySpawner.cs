58c244a8290a8c0ac3dd5075ffb6cb659c822647
Rishi Anand <flywithmerishi16@gmail.com>
2023-03-25 11:38:27 +0530

    first commit

---
+++ EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemy;
+
+    public float timeBetweenWaves = 1f;
+    private float timeToSpawn = 1f;
+
+
+    void Update()
+    {
+        StartSpawning();
+    }
+
+
+    void StartSpawning()
+    {
+        if (Time.time >= timeToSpawn)
+        {
+            SpawnBlock();
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+    }
+
+    void SpawnBlock()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                int randomEnemy = Random.Range(0, enemy.Length);
+                Instantiate(enemy[randomEnemy], spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+
+
+}
---
+++ Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float mapWidth = 5f;
+
+   
+
+    Rigidbody2D _rb;
+
+    private void Start()
+    {
+       _rb =  GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        Vector2 newPosition = _rb.position + Vector2.right * x;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+        _rb.MovePosition(newPosition);
+
+        if (x > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        FindObjectOfType<GameManager>().EndGame();
+        _rb.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+
+}
